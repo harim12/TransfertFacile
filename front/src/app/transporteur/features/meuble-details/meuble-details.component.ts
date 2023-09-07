4c5335569491013208853e746f6be87b5fdf6303
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2023-09-07 10:47:19 +0100

    fixing meuble category bug

--- front/src/app/transporteur/features/meuble-details/meuble-details.component.ts
+++ front/src/app/transporteur/features/meuble-details/meuble-details.component.ts
@@ -4,29 +4,30 @@ import {Component, Input} from '@angular/core';
 export class MeubleDetailsComponent {
     @Input()meubleDemande : any | undefined;
     meubleItems !: Record<string, string[]>
-     ngOnInit() {
+
+    ngOnInit() {
         console.log("this is in meuble demande", this.meubleDemande.specificDemande.items);
-
-        const pattern = /([a-zA-Z\s]+):\{([^}]*)\}/g;
-
-        // Initialize an object to store the categorized items
+    
+        const pattern = /([^:]+):\{([^}]*)\}/g;
+    
         const categorizedItems: Record<string, string[]> = {};
-        
+    
         // Use the regular expression to match and extract category and items
         let match;
         while ((match = pattern.exec(this.meubleDemande.specificDemande.items)) !== null) {
-            const category = match[1];
+            const category = match[1].trim(); // Trim the category name
             const itemsString = match[2];
-        
+    
             // Split the itemsString into individual items and trim them
             const itemsArray = itemsString.split(',').map(item => item.trim());
-        
-            // Store the items in the categorizedItems object
+    
+            // Store the items in the categorizedItems object with the original category name
             categorizedItems[category] = itemsArray;
         }
-        this.meubleItems =categorizedItems;
-        console.log(this.meubleItems)    
+        this.meubleItems = categorizedItems
     }
+    
+    
 
 
 }
